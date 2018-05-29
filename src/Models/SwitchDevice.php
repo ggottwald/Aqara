f30359af6202825aa6129647f4ca14cace6703f1
Götz Gottwald <goetz.gottwald@plentymarkets.com>
2018-05-29 17:12:34 +0200

    ADD switch device

---
+++ src/Models/SwitchDevice.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Aqara\Models;
+
+/**
+ * Class SwitchDevice
+ *
+ * @package Aqara\Models
+ */
+class SwitchDevice extends Subdevice
+{
+    const CLICK              = 'click';
+    const DOUBLE_CLICK       = 'double_click';
+    const LONG_CLICK_PRESS   = 'long_click_press';
+    const LONG_CLICK_RELEASE = 'long_click_release';
+
+    public function __construct(array $attributes = [])
+    {
+        $this->type = 'switch';
+        parent::__construct($attributes);
+    }
+
+    /**
+     * @param array $state
+     */
+    public function handleState($state)
+    {
+        if (!is_array($state)) {
+            return;
+        }
+
+        parent::handleState($state);
+
+        if (isset($state['status'])
+            && in_array($state['status'], [
+                static::CLICK,
+                static::DOUBLE_CLICK,
+                static::LONG_CLICK_PRESS,
+                static::LONG_CLICK_RELEASE,
+            ])) {
+            $this->emit($state['status']);
+        }
+    }
+}
